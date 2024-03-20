1269b410913bead5b197e23860d51d17fef02612
muskan-fatim <160476952+muskan-fatim@users.noreply.github.com>
2024-03-20 00:44:34 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const number = await inquirer.prompt([{
+        name: "first",
+        type: "number",
+        message: "Enter your first number"
+    },
+    {
+        name: "second",
+        type: "number",
+        message: "Enter your second number"
+    },
+    {
+        name: "operators",
+        message: " please choose the operator",
+        type: "list",
+        choices: ["+", "-", "*", "/", "%", "**"]
+    },
+]);
+if (number.operators == "+") {
+    console.log(number.first + number.second);
+}
+else if (number.operators == "-") {
+    console.log(number.first - number.second);
+}
+else if (number.operators == "*") {
+    console.log(number.first * number.second);
+}
+else if (number.operators == "/") {
+    console.log(number.first / number.second);
+}
+else if (number.operators == "%") {
+    console.log(number.first % number.second);
+}
+else if (number.operators == "**") {
+    console.log(number.first ** number.second);
+}
